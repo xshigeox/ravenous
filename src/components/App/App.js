88a5f718828776cdba65ecbe729c64fcfef9d7ba
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-06-29 16:29:45 -0400

    Add business component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./App.css"
+import Business from "../Business/Business"
+
+function App() {
+  return (
+    <div>
+      <Business />
+    </div>
+  )
+}
+
+export default App
